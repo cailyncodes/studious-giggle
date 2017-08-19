0eb2e7943fc37056c3a39d719d81ca817613df2a
Cole Hansen <cole_hansen@brown.edu>
2017-08-19 18:34:18 -0400

    Add first template
    Testing the templating features

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const hapi = require('hapi');
+const hoek = require('hoek');
+const handlebars = require('handlebars');
+const vision = require('vision');
 
 const server = new hapi.Server();
 const port = process.env.PORT;
 const host = "0.0.0.0";
 server.connection({ port, host });
 
-server.route({
-  method: "GET",
-  path: "/",
-  handler: function(req, resp) {
-    resp("Hello World").code(200);
-  }
+server.register(vision, (err) => {
+
+  hoek.assert(!err, err);
+
+  server.views({
+    engines: {
+        html: handlebars
+    },
+    relativeTo: __dirname,
+    path: './templates',
+    helpersPath: './helpers'
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply.view('index', {
+        title: "My Title",
+        content: "<h1>Hello World</h1>"
+      });
+    }
+  });
 });
 
 server.start((err) => {
